185ab3082768f54d4b907fa9534d26856ab726d0
Pantula <sri.sai.k.pantula@accenture.com>
2024-03-17 22:55:51 +0530

    added documentation and made other changes

--- test/pricing.test.js
+++ test/pricing.test.js
@@ -1,21 +1,36 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../server');
 
 describe('Pricing Route', () => {
+    afterAll(async () => {
+       console.log("Test cases done")
+    });
+
     it('should calculate price and return 200 OK', async () => {
         const res = await request(app)
             .post('/pricing')
             .send({
                 zone: 'central',
-                organization_id: '001',
+                organization_id: '005',
                 total_distance: 12,
                 item_type: 'perishable'
             });
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('total_price');
     });
 
-    it('should return 400 Bad Request for invalid input', async () => {
+    it('should calculate price and return 200 OK', async () => {
+        const res = await request(app)
+            .post('/pricing')
+            .send({
+                zone: 'central',
+                organization_id: '005',
+                total_distance: 6,
+                item_type: 'perishable'
+            });
+        expect(res.statusCode).toEqual(200);
+    });
+
+    it('should return 400 Bad Request for invalid total_distance', async () => {
         const res = await request(app)
             .post('/pricing')
             .send({
@@ -28,7 +43,7 @@ describe('Pricing Route', () => {
         expect(res.body).toHaveProperty('errors');
     });
 
-    it('should return 400 Bad Request organisation_is not not there', async () => {
+    it('should return 400 Bad Request for missing organization_id', async () => {
         const res = await request(app)
             .post('/pricing')
             .send({
@@ -38,6 +53,39 @@ describe('Pricing Route', () => {
             });
         expect(res.statusCode).toEqual(400);
         expect(res.body).toHaveProperty('errors');
- });
+    });
+    it('should return 400 Bad Request for missing zone', async () => {
+        const res = await request(app)
+            .post('/pricing')
+            .send({
+                organization_id: 'invalid',
+                total_distance: 20,
+                item_type: 'perishable'
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('errors');
+    });
+    it('should return 400 Bad Request for missing total_distance', async () => {
+        const res = await request(app)
+            .post('/pricing')
+            .send({
+                organization_id: 'invalid',
+                zone: 'central',
+                item_type: 'perishable'
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('errors');
+    });
 
+    it('should return 400 Bad Request for missing item_type', async () => {
+        const res = await request(app)
+            .post('/pricing')
+            .send({
+                organization_id: 'invalid',
+                zone: 'central',
+                total_distance: 6,
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('errors');
+    });
 });
